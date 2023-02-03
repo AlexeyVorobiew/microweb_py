6b3db28562149d1aa38e074c6f1a946c2ecaa719
Alexey Vorobiev <alexey.worobiew@gmail.com>
2023-02-04 01:31:44 +0300

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def main():
     return "<p>Hello, World!</p>"
+
+@app.route("/message", methods = ["POST"])
+def message():
+    posted_data = request.get_json()
+    data = posted_data['data']
+    return  jsonify(str(data))
+
+
+if __name__ == 'main':
+    app.run(debug=True)
